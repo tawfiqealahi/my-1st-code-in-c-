d4d37ec2954636afcd61bc828fdcaa5b4aa00cb0
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-02-06 23:17:10 -0600

    f

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<< "name: tawfiq \n";
-    cout<< "Degree: DVM \n";
-    cout<< "inst: PSTU";
+int marks = 100;
+if (marks <=60){
+    cout<< "starving";
+}
+else if(marks>60 && marks <=80){
+    cout<< "survive";
+}
+else{
+    cout<<"rich ";
+}
 }
