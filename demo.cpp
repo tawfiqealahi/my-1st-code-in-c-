1e36fa595065ffb15e196df78e79c027ec4e0050
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-02-07 12:51:02 -0600

    1

--- demo.cpp
+++ demo.cpp
@@ -1,14 +1,11 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 int main(){
-int marks = 100;
-if (marks <=60){
-    cout<< "starving";
-}
-else if(marks>60 && marks <=80){
-    cout<< "survive";
-}
-else{
-    cout<<"rich ";
+int num = 100;
+if (num%2 == 0){
+    cout<< "even number";
+} else{
+    cout<< "odd";
 }
 }
