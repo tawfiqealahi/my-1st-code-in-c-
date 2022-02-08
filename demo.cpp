0c16fdc3491e60b56b2dfe37dc31d0fb351962b3
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-02-08 06:49:52 -0600

    2

--- demo.cpp
+++ demo.cpp
@@ -2,10 +2,18 @@
 #include<conio.h>
 using namespace std;
 int main(){
-int num = 100;
-if (num%2 == 0){
-    cout<< "even number";
-} else{
-    cout<< "odd";
+int amount;
+cout<< "enter per  ticket price: ";
+cin>> amount;
+int num;
+cout<< "enter total ticket : ";
+cin>>num;
+int totalCost = amount*num;
+if (num>3 || amount>1500){
+    totalCost== totalCost-(totalCost*.15);
 }
+cout<< "you buy " << num << " ticket. \n" << "total TK:" << totalCost ;
+
+
+
 }
