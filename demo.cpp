0e4c9a85edc50e5077d27b5323aea3b4a95e6126
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-02-09 13:49:17 -0600

    5

--- demo.cpp
+++ demo.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main(){
 int i;
 
-    for ( i=0; i<=300 ; ){
-        if(i<100){
-            cout<< (i+=5) << "\n";
-        }else if(i>100 && i<=200){
-         cout<< (i+=10) << "\n";
+    for ( i=0; i<=300 ;i+=5 ){
+       if(i>100 && i<=200){
+         i+=5;
         }else if(i>200 && i<=300){
-         cout<< (i+=20) << "\n";
+         i+=15;
         }
+        cout<< i <<"\n";
     }
 
 
